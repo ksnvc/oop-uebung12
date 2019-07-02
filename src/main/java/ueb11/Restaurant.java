5e1d02f844211d67699539a4715bb3eadb5298ab
Sara Kosanovic <sarakosanovic@Saras-MacBook-Pro.local>
2019-07-02 15:27:20 +0200

    loesung

--- src/main/java/ueb11/Restaurant.java
+++ src/main/java/ueb11/Restaurant.java
@@ -2,12 +2,19 @@ package ueb11;
 
 public class Restaurant {
     public static void main(String[]args){
+
         MyDurchreiche durchreiche = new MyDurchreiche(3);
+
         Koch hans = new Koch("Hans", 3, durchreiche);
+        Koch albert = new Koch("Albert", 2, durchreiche);
         Bedienung bernd = new Bedienung("Bernd", 3, durchreiche);
+        Bedienung peter = new Bedienung("Peter", 2, durchreiche);
 
         new Thread(hans).start();
+        new Thread(albert).start();
         new Thread(bernd).start();
+        new Thread(peter).start();
+
 
     }
 
